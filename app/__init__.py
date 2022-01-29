b1109f46bdd13aac4f2cd025cfd2cd09da538485
ivymmurithi <ivymurithi@gmail.com>
2022-01-29 10:07:33 +0300

    initialize the application

--- app/__init__.py
+++ app/__init__.py
@@ -1,1 +1,9 @@
 from flask import Flask
+from .config import config_options
+
+app = Flask(__name__,instance_relative_config= True)
+
+app.config.from_object(config_options['development'])
+app.config.from_pyfile('config.py')
+
+from app import views
