3e0fafef1f32a4a6475b5b658900dea675019a4b
ivymmurithi <ivymurithi@gmail.com>
2022-01-29 10:02:08 +0300

    add congigurations and api base url

---
+++ app/config.py
@@ -0,0 +1,32 @@
+class Config:
+    '''
+    General configuration parent class
+    '''
+    NEWS_API_BASE_URL = 'https://newsapi.org/v2/everything?q={}&from=2022-01-01&language=en&sortBy=popularity&apiKey={}'
+
+
+
+class ProdConfig(Config):
+    '''
+    Production  configuration child class
+
+    Args:
+        Config: The parent configuration class with General configuration settings
+    '''
+    pass
+
+
+class DevConfig(Config):
+    '''
+    Development  configuration child class
+
+    Args:
+        Config: The parent configuration class with General configuration settings
+    '''
+
+    DEBUG = True
+
+config_options = {
+  'development':DevConfig,
+  'production': ProdConfig
+}
